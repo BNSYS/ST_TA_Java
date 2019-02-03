859fd27a483fcc7a328cb1a1d6d602deb0dec97e
Andrey Sokolovskiy <bugnetsys@gmail.com>
2019-02-03 14:13:54 +0300

    Functions added

--- sandbox/src/main/java/ru/stqa/st_ta_java/sandbox/BlockDemo.java
+++ sandbox/src/main/java/ru/stqa/st_ta_java/sandbox/BlockDemo.java
@@ -5,11 +5,19 @@ For demo
 */
 class BlockDemo {
   public static void main(String args[]) {
+    countToFive();
+    System.out.println("Ready");
+    System.out.println(isBelowZeroCelsius(280));
+  }
+  public static void countToFive () {
     int count;
     for (count = 0; count < 5; count++) {
       System.out.println("Counter value: " + count);
       System.out.println("NotReady");
     }
-    System.out.println("Ready");
+  }
+
+  public static boolean isBelowZeroCelsius(double kelvin){
+    return !(kelvin < 273);
   }
 }
