b8106eb4dd27405313a426f35d7799e0e21e125a
Andrey Sokolovskiy <bugnetsys@gmail.com>
2019-02-03 09:38:00 +0300

    Test project Git Gradle IDEA

---
+++ src/main/java/BlockDemo.java
@@ -0,0 +1,13 @@
+/*
+For demo
+*/
+class BlockDemo {
+	public static void main(String args[]) {
+	int count;
+	for (count=0; count<5; count++){ 
+	 System.out.println("Counter value: " + count);
+	 System.out.println("NotReady");
+	}
+	System.out.println("Ready");
+	}
+}
